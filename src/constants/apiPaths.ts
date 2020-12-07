182f6787997f07bf82630ce23ffb075dfd382ff2
Gelelus <mundor7392290@gmail.com>
2020-12-07 19:23:21 +0300

    update task8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,6 +4,7 @@ const API_PATHS = {
   order: 'https://z84l00398g.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ejmdwhtdmc.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://z84l00398g.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://gelelus-cart-api-dev.eu-west-1.elasticbeanstalk.com/',
 };
 
 export default API_PATHS;
